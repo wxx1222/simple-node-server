649c57e3a1c9e904103870019099113dd1160960
wangxiang <15389122543@163.com>
2019-04-02 19:18:25 +0800

    删除无用代码

--- app.js
+++ app.js
@@ -10,15 +10,8 @@ const cors = require('cors')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/' , (req,res) => {
-    console.log('请求过来了')
-    res.send('返回数据')
-    console.log(path.dirname('index.json'))
-    console.log(path.join(__dirname, '111'))
-    console.log(path.join(__dirname,'index.json'))
-
-})
 app.options('*', cors())
+
 let  writeToJsonFile = (fileName, fileData, callback) => {
     fs.writeFile(path.join(__dirname, fileName), fileData , (err, data) => {
         callback(err, data)
@@ -38,51 +31,20 @@ app.get('/getZh', cors(), ( req,res) => {
 })
 
 app.post('/postZh', cors(), (req,res) => {
-    console.log('postZh')
     // console.log(req.body)
     let postData = JSON.stringify(req.body)
     writeToJsonFile('zh.json', postData, (err, data) => {
-        if( err ) res.json({code:0,massage:'保存失败'})
-        res.json({code:0,massage:'保存成功'})
+        if( err ) res.json({code:0,message:'保存失败'})
+        res.json({code:0,message:'保存成功'})
     })
 })
 app.post('/postEn', cors(), (req,res) => {
-    console.log('postEn')
     // console.log(req.body)
     let postData = JSON.stringify(req.body)
     writeToJsonFile('en.json' ,postData,  (err, data) => {
-        if( err ) res.json({code:0,massage:'保存失败'})
-        res.json({code:0,massage:'保存成功'})
+        if( err ) res.json({code:0,message:'保存失败'})
+        res.json({code:0,message:'保存成功'})
     })
-})
-app.post('/editEn', cors(), (req, res) => {
-    console.log('editEn')
-    // console.log(req.body)
-    let postData = JSON.stringify(req.body)
-    writeToJsonFile('en.json', postData, (err, data) => {
-        if (err) res.json({ code: 0, massage: '保存失败' })
-        res.json({ code: 0, massage: '保存成功' })
-    })
-})
-app.post('/editZh', cors(), (req, res) => {
-    console.log('editZh')
-    // console.log(req.body)
-    let postData = JSON.stringify(req.body)
-    writeToJsonFile('zh.json', postData, (err, data) => {
-        if (err) res.json({
-            code: 0,
-            massage: '保存失败'
-        })
-        res.json({
-            code: 0,
-            massage: '保存成功'
-        })
-    })
-})
-app.post('/post', cors(), (req,res) => {
-    let postData = JSON.stringify(req.body)
-    console.log('post请求过来了', postData)
-    res.send('返回post数据')
 })
 
 app.listen(1234, () => {
